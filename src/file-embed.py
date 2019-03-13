220cccb5bdaf936506e55ff5f3aaf2a5743651bf
soojihan <shan12@sheffield.ac.uk>
2019-03-13 01:39:55 +0000

    elmo cred avg run on pheme data on andromeda

--- src/file-embed.py
+++ src/file-embed.py
@@ -36,38 +36,43 @@ import nltk
 from nltk.corpus import stopwords
 from nltk import WordNetLemmatizer
 from ast import literal_eval
+import argparse
 
-# _stop_words = stopwords.words('english')
-# stop_words_filter = lambda t : filter(lambda a: a not in _stop_words, t)
-# punctuation_filter = lambda t : filter(lambda a: a not in string.punctuation, t)
-#
-# lemmatizer = WordNetLemmatizer()
-# postag_map = {'j': 'a','n': 'n','v': 'v'}
-#
-# def _lemmatize(token_list):
-#     lemmatized_tokens = [lemmatizer.lemmatize(token, postag_map[tag[0].lower()]) if tag[0].lower() in ['j', 'n', 'v']
-#                         else lemmatizer.lemmatize(token) for token, tag in nltk.pos_tag(token_list)]
-#     return lemmatized_tokens
+parser = argparse.ArgumentParser()
+parser.add_argument('--event', help='the name of event')
+parser.add_argument('--infile_cand', help='path to candidates')
+parser.add_argument('--infile_ref', help='path to ref')
+parser.add_argument('--outfile_cand', help='path to store cand embedding')
+parser.add_argument('--outfile_ref', help='path to store ref embedding')
+# print(parser.format_help())
+
+args = parser.parse_args()
+event = args.event
+infile_cand = args.infile_cand
+infile_ref = args.infile_ref
+outfile_cand = args.outfile_cand
+outfile_ref = args.outfile_ref
 
 
 pd.set_option('display.max_columns', None)
 pd.options.mode.chained_assignment = None
 
-event = 'boston'
+# event = 'boston'
 
 # options_file = "https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway/elmo_2x4096_512_2048cnn_2xhighway_options.json"
 # weight_file = "https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway/elmo_2x4096_512_2048cnn_2xhighway_weights.hdf5"
-options_file = "https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway_5.5B/elmo_2x4096_512_2048cnn_2xhighway_5.5B_options.json"
-weight_file = "https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway_5.5B/elmo_2x4096_512_2048cnn_2xhighway_5.5B_weights.hdf5"
+# options_file = "https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway_5.5B/elmo_2x4096_512_2048cnn_2xhighway_5.5B_options.json"
+# weight_file = "https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway_5.5B/elmo_2x4096_512_2048cnn_2xhighway_5.5B_weights.hdf5"
 
-elmo_credbank_model_path = os.path.join(os.path.dirname(__file__), '..', '..', 'rumourdnn', "resource", "embedding",
-                                        "elmo_model", "weights_12262018.hdf5")
+# elmo_credbank_model_path = os.path.join(os.path.dirname(__file__), '..', '..', 'rumourdnn', "resource", "embedding",
+#                                         "elmo_model", "weights_12262018.hdf5")
+elmo_credbank_model_path = '/oak01/data/sooji/data-aug/resource/weights_12262018.hdf5'
 elmo = ElmoEmbedder(
-    # options_file="https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway_5.5B/elmo_2x4096_512_2048cnn_2xhighway_5.5B_options.json",
-    # weight_file=elmo_credbank_model_path)
-    options_file= options_file,
-    weight_file= weight_file)
-
+    options_file="https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway_5.5B/elmo_2x4096_512_2048cnn_2xhighway_5.5B_options.json",
+    weight_file=elmo_credbank_model_path)
+    # options_file= options_file,
+    # weight_file= weight_file)
+#
 
 def load_semeval_data():
     data_type = "merged_semeval"
@@ -114,10 +119,16 @@ def load_pheme_data():
 
 
 def prepare_input_file():
+    """
+    Generate input files where each line contains a sentence tokenized by whitespace.
+    :return:
+    """
     # data = load_semeval_data()
     ref, data = load_pheme_data()
     # outfile = os.path.join(os.path.dirname(__file__), '..', 'data/semeval2015/file-embed-input')
-    outfile = os.path.abspath(os.path.join(os.path.dirname(__file__), '..',  'data_augmentation/data/file-embed-input'))
+    outfile = os.path.abspath(os.path.join(os.path.dirname(__file__), '..',  'data_augmentation/data/file-embed-input/{}'.format(event)))
+    outfile = os.path.abspath(outfile)
+    os.makedirs(outfile, exist_ok=True)
     print(outfile)
 
     # tokenised_tweet1 = list(map(lambda x: " ".join(literal_eval(x)), data['processed_tweet1'].values))
@@ -129,34 +140,132 @@ def prepare_input_file():
     tokenised_tweet2 = list(map(lambda x: " ".join(x), processed_ref))
     # pp(tokenised_tweet1[:10])
     for t in tokenised_tweet1:
-        with open(os.path.join(outfile, 'input-cand.txt'), 'a') as f:
-        # with open(os.path.join(outfile, 'input-text1.txt'), 'a') as f:
+        # with open(os.path.join(outfile, 'input-cand.txt'), 'a') as f:
+        with open(os.path.join(outfile, 'input-text1.txt'), 'a') as f:
             f.write(t)
             f.write("\n")
     f.close()
 
     for t in tokenised_tweet2:
-        with open(os.path.join(outfile, 'input-ref.txt'), 'a') as f:
-        # with open(os.path.join(outfile, 'input-text2.txt'), 'a') as f:
+        # with open(os.path.join(outfile, 'input-ref.txt'), 'a') as f:
+        with open(os.path.join(outfile, 'input-text2.txt'), 'a') as f:
             f.write(t)
             f.write("\n")
     f.close()
-
+    print("done")
     # with open(os.path.join(outfile, 'input-text2.txt'), 'r') as f:
     #     x = f.readlines()
     # pp(x)
 
-# infile = os.path.join(os.path.dirname(__file__), '..', 'data/semeval2015/data/file-embed-input/input-text1.txt')
-# outfile = os.path.join(os.path.dirname(__file__), '..', 'data/semeval2015/data/file-embed-output/output-text1.hdf5')
-# elmo.embed_file(input_file=infile, output_file_path=outfile, output_format='average', forget_sentences=True)
-#
-# infile = os.path.join(os.path.dirname(__file__), '..', 'data/semeval2015/data/file-embed-input/input-text2.txt')
-# outfile = os.path.join(os.path.dirname(__file__), '..', 'data/semeval2015/data/file-embed-output/output-text2.hdf5')
-# elmo.embed_file(input_file=infile, output_file_path=outfile, output_format='average', forget_sentences=True)
+def remove_empty_lines_from_input(indices):
+    """
 
-prepare_input_file()
+    :param indices: indices of empty lines
+    :return:
+    """
+    outfile = os.path.abspath(os.path.join(os.path.dirname(__file__), '..',  'data_augmentation/data/file-embed-input/{}'.format(event)))
+
+    with open(os.path.join(outfile, 'input-text1.txt'), 'r') as f:
+        x = f.read().splitlines()
+        lines = [k for k in x if k]
+    f.close()
+    print(len(lines))
+    print(len(indices))
+    print(len(x))
+    assert len(x) == (len(lines)+len(indices))
+
+    for t in lines:
+        with open(os.path.join(outfile, 'input-text1-noempty.txt'), 'a') as f:
+            f.write(t)
+            f.write("\n")
+        f.close()
+
+def semeval_sem_sim():
+    text1_emb = os.path.join(os.path.dirname(__file__), '..', 'data/semeval2015/file-embed-output/5.5b-avg/output-text1.hdf5')
+    text2_emb = os.path.join(os.path.dirname(__file__), '..', 'data/semeval2015/file-embed-output/5.5b-avg/output-text2.hdf5')
+    f1 = h5py.File(text1_emb, 'r')
+    print("Keys: %s" % len(f1.keys()))
+
+    f2 = h5py.File(text2_emb, 'r')
+    print("Keys: %s" % len(f2.keys()))
+    sim_scores=[]
+    tweet_id = []
+    gold_labels= []
+    pp(f1.keys())
+    data = load_semeval_data()
+    print(list(data))
+    for i, k in enumerate(f1.keys()):
+        if i%500==0:
+            print(i)
+
+        if k != 'sentence_to_index':
+            int_id = int(k)
+            label = data.loc[int_id]['goldlabel']
+            gold_labels.append(label)
+            text1 = np.average(f1[(k)], axis=0).reshape(1,-1)
+            text2 = np.average(f2[(k)], axis=0).reshape(1,-1)
+            assert text1.shape[1]==text2.shape[1]
+
+            if not (np.isnan(text1).any()) and not (np.isnan(text2).any()):
+                score = cosine_similarity(text1, text2)
+                sim_scores.append(score.flatten()[0])
+                tweet_id.append(k)
+                # if score.flatten()[0] >= 0.673580:
+                #     print(i, cand_id, score.flatten()[0])
+            else:
+                score = 0
+                sim_scores.append(0)
+                tweet_id.append(k)
+            # if i==10:
+            #     break
+    df = pd.DataFrame()
+    df['sim_score']=sim_scores
+    df['id']=tweet_id
+    df['label']=gold_labels
+    print(df.head())
+    df.sort_values(by=['id'],ascending=True)
+    print(df.head())
+    outfile = os.path.abspath(os.path.join(os.path.dirname(__file__), '..',  'data/semeval2015/file-embed-output/5.5b-avg'))
+    df.to_csv(os.path.join(outfile,'score.csv'))
+
+def eval_results():
+    result_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..',  'data/semeval2015/file-embed-output/5.5b-avg/score.csv'))
+    print(result_path)
+    eval(result_path)
+
+
 #
+# # infile = os.path.join(os.path.dirname(__file__), '..', 'data/semeval2015/file-embed-input/input-text1.txt')
+# infile = os.path.join(os.path.dirname(__file__), '..', 'data_augmentation/data/file-embed-input/boston/input-text1.txt')
+# infile = os.path.abspath(infile)
+# # outfile = os.path.join(os.path.dirname(__file__), '..', 'data/semeval2015/file-embed-output/5.5b-avg/output-text1.hdf5')
+# outfile = os.path.join(os.path.dirname(__file__), '..', 'data_augmentation/data/file-embed-output/boston/output-text1.hdf5')
+# outfile = os.path.abspath(outfile)
+# print(outfile)
+# elmo.embed_file(input_file=infile, output_file_path=outfile, output_format='average')
+#
+# # infile = os.path.join(os.path.dirname(__file__), '..', 'data/semeval2015/file-embed-input/input-text2.txt')
+# infile = os.path.join(os.path.dirname(__file__), '..', 'data_augmentation/data/file-embed-input/boston/input-text2.txt')
+#
+# # outfile = os.path.join(os.path.dirname(__file__), '..', 'data/semeval2015/file-embed-output/5.5b-avg/output-text2.hdf5')
+# outfile = os.path.join(os.path.dirname(__file__), '..', 'data_augmentation/data/file-embed-output/boston/output-text2.hdf5')
+#
+# elmo.embed_file(input_file=infile, output_file_path=outfile, output_format='average')
+
+
+## andromeda
+elmo.embed_file(input_file=infile_cand, output_file_path=outfile_cand, output_format='average')
+elmo.embed_file(input_file=infile_ref, output_file_path=outfile_ref, output_format='average')
+
+#######################################################################################################################################
+# semeval_sem_sim()
+# eval_results()
+# prepare_input_file()
+# remove_empty_lines_from_input(boston_empty)
 # f = h5py.File(outfile, 'r')
 # print("Keys: %s" % len(f.keys()))
 # print(f[('1')].shape)
 # f.close()
+
+#### TODO: boston missing indices --->
+# boston_empty = [51854, 256585, 296905, 395193, 415348, 417639]

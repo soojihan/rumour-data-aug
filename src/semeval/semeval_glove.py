f2e9ce053286b1ae6e158db1d50ede7e55bb4f23
soojihan <shan12@sheffield.ac.uk>
2019-03-19 18:28:15 +0000

    clean repo

---
+++ src/semeval/semeval_glove.py
@@ -0,0 +1,13 @@
+Unstaged changes after reset:
+M	src/file-embed.py
+D	src/retweet_collector.py
+D	src/retweet_processor.py
+D	src/sem_sim_elmo_proto.py
+M	src/semeval/semeval_data_processor.py
+M	src/semeval/semeval_elmo.py
+M	src/semeval/semeval_glove.py
+D	src/semeval_data_processor.py
+D	src/semeval_elmo.py
+D	src/semeval_glove.py
+M	src/simsem_eval.py
+D	src/twint_features.py

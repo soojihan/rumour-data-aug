8a57ba2399409424ab690cf3fa1064829718867c
soojihan <shan12@sheffield.ac.uk>
2019-03-19 18:24:18 +0000

    delete andromeda shell scripts

--- src/andromeda/annotation.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#nohup /oak1/data/sooji/anaconda3/envs/py3/bin/python3.6 -u /oak01/data/sooji/data_annotation/src/preprocessing.py > /oak01/data/sooji/data_annotation/src/preprocessing.log 2>&1 &
-nohup ./anaconda3/envs/py3/bin/python3.6 -u /oak01/data/sooji/data_annotation/src/annotation.py > /oak01/data/sooji/data_annotation/src/annotation.log 2>&1 &
-
-
-
-
--- src/andromeda/batch_annotation7.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#nohup /oak1/data/sooji/anaconda3/envs/py3/bin/python3.6 -u /oak01/data/sooji/data_annotation/src/preprocessing.py > /oak01/data/sooji/data_annotation/src/preprocessing.log 2>&1 &
-nohup ./anaconda3/envs/py3/bin/python3.6 -u /oak01/data/sooji/data_annotation/src/batch_annotation.py 7 > /oak01/data/sooji/data_annotation/train_augmentation/batch_annotation7.log 2>&1 &
-
-
-
-

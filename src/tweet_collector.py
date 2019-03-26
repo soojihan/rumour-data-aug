e2adefc533caf2d35dfc572f3f62e2455f6f6230
soojihan <shan12@sheffield.ac.uk>
2019-03-26 15:19:43 +0000

    add optionparser

--- src/tweet_collector.py
+++ src/tweet_collector.py
@@ -10,6 +10,7 @@ import argparse
 import time
 import pickle
 from ast import literal_eval
+from optparse import OptionParser
 
 """
 Retreive the top 100 retweets of each context tweet in the Pheme data
@@ -17,16 +18,6 @@ Retreive the top 100 retweets of each context tweet in the Pheme data
 
 pd.set_option('display.expand_frame_repr', False)
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--event', help='the name of event')
-parser.add_argument('--df_path', help='path to tweet ids which will be downloaded')
-# parser.add_argument('--save_path', help='path to save downloaded tweets')
-
-args = parser.parse_args()
-event = args.event
-df_path = args.df_path
-print(df_path)
-# save_path = args.save_path
 
 class twitter_configuration:
     def __init__(self):
@@ -50,7 +41,6 @@ class twitter_configuration:
 t = twitter_configuration()
 c = t.config(lib_type='twint')
 
-
 def search_tweets(keyword, since_t, until_t, output_path):
     c.Since = since_t
     c.Until = until_t
@@ -58,17 +48,15 @@ def search_tweets(keyword, since_t, until_t, output_path):
     c.Search = keyword
     c.Store_json = True
     c.User_full = True
-    c.Get_replies = True
-    c.Replies = True
     c.Lang = 'en'
     c.Output = os.path.join(output_path, "{}.json".format(keyword))
-    c.Format = "Tweet id: {id} | Timezone: {timezone} | Time: {time} | Date: {date} | Tweet: {tweet} | replies: {replies}"
+    c.Format = "Tweet id: {id} | Timezone: {timezone} | Time: {time} | Date: {date} | Tweet: {tweet} | Replies: {replies}"
     twint.run.Search(c)
 
-def collect_new_event():
+def collect_new_event(keywords, output_path, since, until):
     start = time.time()
-    keywords = ['#ChristchurchTerrorAttack', '#christchurch', '#NewZealandMosqueAttack', '#NewZealandShooting', 'christchurch', 'new zealand']
-    output_path = os.path.join('..', 'data_augmentation/christichurch-shooting')
+    # keywords = ['#ChristchurchTerrorAttack', '#christchurch', '#NewZealandMosqueAttack', '#NewZealandShooting', 'christchurch', 'new zealand']
+    # output_path = os.path.join('..', 'data_augmentation/christichurch-shooting')
     os.makedirs(output_path, exist_ok=True)
 
     for i, keyword in enumerate(keywords):
@@ -78,12 +66,39 @@ def collect_new_event():
             print("already exists...")
             continue
         else:
-            tweets = search_tweets(keyword, since_t='2019-03-15', until_t='2019-03-17', output_path=output_path)
+            # tweets = search_tweets(keyword, since_t='2019-03-15', until_t='2019-03-17', output_path=output_path)
+            tweets = search_tweets(keyword=keyword, since_t=since, until_t=until, output_path=output_path)
             pp(tweets)
     end = time.time()
     print("{}".format(time.strftime("%H:%M:%S", time.gmtime(end-start))))
 
-collect_new_event()
+def main():
+    parser = OptionParser()
+    parser.add_option(
+        '--keywords', dest='keywords', default=['#ChristchurchTerrorAttack'],
+        help='List of keywords for searching tweets (list): default=%default')
+    parser.add_option(
+        '--output', dest='output', default='/path/to/store/output',
+        help='Path to save downloaded tweets (string) : default=%default')
+    parser.add_option(
+        '--since', dest='since', default='2019-03-26',
+        help='Filter tweets sent since date (string): default=%default')
+    parser.add_option(
+        '--until', dest='until', default='2019-03-27',
+        help='Filter tweets sent until date (string): default=%default')
+
+    (options, args) = parser.parse_args()
+    keywords = options.keywords
+    keywords = literal_eval(keywords)
+    print("keywords: {}".format(keywords))
+    output_path = options.output
+    since = str(options.since)
+    until = str(options.until)
+
+    collect_new_event(keywords, output_path, since, until)
+
+
+main()
 
 
 
